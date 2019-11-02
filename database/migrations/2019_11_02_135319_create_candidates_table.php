90456526edfc4401001ece34bfedb1ee0e00cf8a
Gustavo Sena <gustavosena98@LIVE.COM>
2019-11-02 11:27:58 -0300

    Criação da migração tabela candidates

---
+++ database/migrations/2019_11_02_135319_create_candidates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->increments('id_candidates'); //
+            $table->string('name'); //Nome do candidato
+            $table->date('date_of_birth'); //Data de nascimento do candidato
+            $table->string('zip_code'); //Código de Endereçamento Postal do candidato
+            $table->string('city'); //Cidade do Candidato
+            $table->string('state'); //Estado do Candidato
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
